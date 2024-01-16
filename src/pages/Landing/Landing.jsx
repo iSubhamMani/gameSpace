05ca888aee475b99829566a082f915b2ece89a3a
Subham Mani <manisubham09@gmail.com>
2024-01-16 16:08:11 +0530

    display carousel

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,5 +1,20 @@
+import Carousel from "../../components/Carousel/Carousel";
+import carouselData from "../../utils/carouselData";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className="px-6 py-8">
+      <div className="flex justify-center items-center">
+        <div className="max-w-[1280px] rounded-lg overflow-hidden">
+          <Carousel>
+            {carouselData.map((carousel) => (
+              <img src={carousel.imageUrl} key={carousel.id} alt="" />
+            ))}
+          </Carousel>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
---
+++ src/utils/carouselData.js
@@ -0,0 +1,26 @@
+const carouselData = [
+  {
+    imageUrl: "https://www.bognor.news/wp-content/uploads/2023/12/GTA-5_-1.jpg",
+    id: 123,
+  },
+  {
+    imageUrl:
+      "https://sm.ign.com/ign_za/screenshot/default/witcher-3-blogroll_srcr.jpg",
+    id: 4782,
+  },
+  {
+    imageUrl: "https://i.ytimg.com/vi/Dw_oH5oiUSE/maxresdefault.jpg",
+    id: 5235,
+  },
+  {
+    imageUrl:
+      "https://images.hdqwalls.com/download/call-of-duty-modern-warfare-remastered-2019-4k-ow-1280x720.jpg",
+    id: 889,
+  },
+  {
+    imageUrl: "https://beanstalk.io/wp-content/uploads/2022/11/1173124.jpg",
+    id: 1892,
+  },
+];
+
+export default carouselData;
