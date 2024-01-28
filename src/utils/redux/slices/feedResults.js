99c562d87c73443218f9eabab8747ade1aa032d0
Subham Mani <manisubham09@gmail.com>
2024-01-28 13:30:06 +0530

    track next page state

--- src/utils/redux/slices/feedResults.js
+++ src/utils/redux/slices/feedResults.js
@@ -4,13 +4,22 @@ const feedResults = createSlice({
   name: "feed",
   initialState: {
     results: [],
+    pageNumber: 1,
+    nextPage: true,
   },
   reducers: {
     addResults: (state, action) => {
       state.results.push(...action.payload);
     },
+    updatePageNumber: (state) => {
+      state.pageNumber = state.pageNumber + 1;
+    },
+    setNextPage: (state, action) => {
+      state.nextPage = action.payload;
+    },
   },
 });
 
-export const { addResults } = feedResults.actions;
+export const { addResults, updatePageNumber, setNextPage } =
+  feedResults.actions;
 export default feedResults.reducer;
