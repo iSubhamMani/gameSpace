0424bdaae233a2d8172ddd875f7aa2719a64a298
Subham Mani <manisubham09@gmail.com>
2024-02-04 15:47:57 +0530

    remove filter

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import Feed from "../../components/Feed/Feed";
-import Filter from "../../components/Filter/Filter";
 
 const Home = () => {
   return (
     <div
-      className="px-4 sm:px-8 py-4 flex flex-col md:flex-row gap-6 justify-between
+      className="px-4 sm:px-8 py-4
     dark:bg-gradient-to-l from-[#11111ff3] to-secondary-color-dark"
     >
-      <Filter />
       <Feed />
     </div>
   );
