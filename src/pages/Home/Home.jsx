fce5d3afdf1ea4327ca376359cb9544792c52eda
Subham Mani <manisubham09@gmail.com>
2024-02-07 00:44:34 +0530

    bug/fix: search input

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,23 @@
-import { useEffect, useState } from "react";
 import Feed from "../../components/Feed/Feed";
 import SearchResults from "../../components/SearchResults/SearchResults";
 import search from "../../assets/search.png";
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchQuery } from "../../utils/redux/slices/search";
 
 const Home = () => {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [searchInput, setSearchInput] = useState("");
+  const { searchQuery } = useSelector((store) => store.search);
+  const dispatch = useDispatch();
 
   const handleSearch = () => {
-    setSearchQuery(searchInput);
+    const searchInput = document.getElementById("search-input");
+    const searchText = searchInput.value;
+
+    dispatch(setSearchQuery(searchText));
   };
 
-  useEffect(() => {
-    if (searchInput.trim() === "") setSearchQuery("");
-  }, [searchInput]);
+  const handleEmptySearch = (e) => {
+    if (e.target.value === "") dispatch(setSearchQuery(""));
+  };
 
   return (
     <div
@@ -22,9 +26,10 @@ const Home = () => {
     >
       <div className="px-6 py-2 flex gap-3 justify-between w-full sm:max-w-[400px] my-8 items-center border-2 border-primary-color rounded-full">
         <input
+          id="search-input"
           type="text"
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
+          defaultValue={searchQuery}
+          onChange={(e) => handleEmptySearch(e)}
           placeholder="Search games"
           className="w-[80%] bg-transparent text-lg text-primary-color focus:outline-none"
         />
