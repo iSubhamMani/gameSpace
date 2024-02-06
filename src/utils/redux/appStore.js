afe50b57fa92b58f3078611d736ba19f8c4bf147
Subham Mani <manisubham09@gmail.com>
2024-02-07 00:44:06 +0530

    add search cache

--- src/utils/redux/appStore.js
+++ src/utils/redux/appStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./slices/userSlice";
 import feedResults from "./slices/feedResults";
+import search from "./slices/search";
 
 const appStore = configureStore({
   reducer: {
     user: userSlice,
     feed: feedResults,
+    search: search,
   },
 });
 
