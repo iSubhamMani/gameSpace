703cd27b1ea86a1c33eed109560434e4a73c2ed4
Subham Mani <manisubham09@gmail.com>
2024-02-09 20:28:11 +0530

    feature: add items to cart

---
+++ src/components/Cart/CartDetails.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setHasSearchCache } from "../../utils/redux/slices/search";
+import { setHasFeedCache } from "../../utils/redux/slices/feed";
+import CartItem from "./CartItem";
+
+const CartDetails = () => {
+  const dispatch = useDispatch();
+  const { searchQuery } = useSelector((store) => store.search);
+  const { cartItems } = useSelector((store) => store.cart);
+
+  useEffect(() => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+    if (searchQuery) dispatch(setHasSearchCache(true));
+    dispatch(setHasFeedCache(true));
+  }, []);
+
+  return cartItems.length === 0 ? (
+    <div className="py-6 px-4 bg-bg-secondary-clr-light dark:bg-bg-secondary-clr-dark flex justify-center min-h-[80vh]">
+      <p className="text-black dark:text-text-clr-primary font-medium text-lg sm:text-2xl">
+        Cart empty...Add some games to your life!
+      </p>
+    </div>
+  ) : (
+    <div className="min-h-[80vh] px-4 py-8 bg-bg-secondary-clr-light dark:bg-bg-secondary-clr-dark ">
+      <div className="games-container md:flex-1 gap-6 md:gap-7">
+        {cartItems.map((item) => {
+          return <CartItem game={item} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CartDetails;
---
+++ src/components/Cart/CartItem.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Trash } from "react-feather";
+import { useDispatch } from "react-redux";
+import { deleteCartItem } from "../../utils/redux/slices/cart";
+
+const CartItem = ({ game }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div
+        className="bg-[#f8f8f8] shadow-2xl dark:bg-primary-color-dark rounded-t-2xl overflow-hidden
+      relative"
+      >
+        <div className="h-[12rem] sm:h-[18rem] md:h-[20rem]">
+          <img
+            className="w-full h-full object-cover"
+            src={game?.background_image}
+            alt=""
+          />
+        </div>
+        <div className="absolute bottom-0 left-0 px-4 pb-4 pt-2 w-full bg-gradient-to-t from-[#000] to-transparent flex gap-2 justify-between items-center">
+          <h3 className="text-text-clr-primary font-bold text-lg sm:text-xl line-clamp-1">
+            {game?.name}
+          </h3>
+        </div>
+      </div>
+      <div>
+        <button
+          onClick={() => dispatch(deleteCartItem(game.id))}
+          className="hover:opacity-90 flex gap-2 justify-center items-center rounded-b-2xl font-bold bg-red-600 text-text-clr-primary w-full py-3"
+        >
+          <span>Remove from cart</span>
+          <Trash width={18} height={18} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
