fc4493eaf3e93cefaa5efd1610f60128e0dea540
Subham Mani <manisubham09@gmail.com>
2024-02-09 11:26:47 +0530

    feature: get randomly generated prices

--- src/utils/getPrice.js
+++ src/utils/getPrice.js
@@ -1,6 +1,6 @@
 const getPrice = (rating) => {
   if (rating >= 4) {
-    return Math.floor(Math.random() * (70 - 40) + 40) + 0.99;
+    return Math.floor(Math.random() * (60 - 40) + 40) + 0.99;
   } else if (rating < 4) {
     return Math.floor(Math.random() * (40 - 20) + 20) + 0.99;
   }
