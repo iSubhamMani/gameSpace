d2602394fd9d1567e20589a43284ff4433c0d46e
Subham Mani <manisubham09@gmail.com>
2024-02-10 11:37:21 +0530

    feature: add cart items indicator

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,11 +5,17 @@ import { Link } from "react-router-dom";
 
 const Cart = () => {
   const user = useSelector((store) => store.user);
+  const { cartItems } = useSelector((store) => store.cart);
 
   return user?.userInfo ? (
     <Link to="/cart">
-      <div className="hover:scale-90 transition duration-200 ease-in-out">
+      <div className="hover:scale-90 transition duration-200 ease-in-out relative">
         <img className="w-5 sm:w-7" src={cart} alt="" />
+        {cartItems.length === 0 ? null : (
+          <span className="px-[0.3rem] py-0 sm:px-2 sm:py-[0.08rem] text-[0.6rem] sm:text-[0.8rem] rounded-full text-white font-bold bg-primary-color absolute -top-1 -right-1 sm:-top-2 sm:-right-2">
+            {cartItems.length}
+          </span>
+        )}
       </div>
     </Link>
   ) : null;
