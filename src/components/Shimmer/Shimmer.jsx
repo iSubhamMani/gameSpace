eb18e9b9e1f38c0dd5e9fa38f16858e23facdb58
Subham Mani <manisubham09@gmail.com>
2024-02-13 00:57:29 +0530

    update shimmer styles

--- src/components/Shimmer/Shimmer.jsx
+++ src/components/Shimmer/Shimmer.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 
 const Shimmer = () => {
+  const { theme } = useSelector((store) => store.app);
+
+  const shimmerRef = useRef();
+
+  useEffect(() => {
+    if (theme === "dark") {
+      shimmerRef.current.classList.remove("shimmer");
+      shimmerRef.current.classList.add("shimmer-dark");
+    } else {
+      shimmerRef.current.classList.add("shimmer");
+      shimmerRef.current.classList.remove("shimmer-dark");
+    }
+  }, [theme]);
+
   return (
     <div className="rounded-2xl overflow-hidden shadow-md">
-      <div className="h-[12rem] sm:h-[18rem] md:h-[20rem] bg-[#b8b8c5] dark:bg-[#3e3e45]"></div>
+      <div
+        ref={shimmerRef}
+        className="h-[12rem] sm:h-[18rem] md:h-[20rem]"
+      ></div>
     </div>
   );
 };
