cc29fac8561251d3617226cf587ed20089ab1fbe
Subham Mani <manisubham09@gmail.com>
2024-02-12 13:49:04 +0530

    design footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,12 @@
 const Footer = () => {
   return (
     <footer className="bg-bg-primary-clr-light dark:bg-bg-primary-clr-dark">
-      Footer
+      <div className="px-4 py-3 text-center">
+        <p className="text-sm font-bold text-black dark:text-text-clr-primary">
+          Created with ❤️ by{" "}
+          <a href="https://github.com/iSubhamMani">Subham Mani</a>
+        </p>
+      </div>
     </footer>
   );
 };
