5a94f75fb62b72719ae1555e41440a6459487c4d
Subham Mani <manisubham09@gmail.com>
2024-02-12 23:33:59 +0530

    update styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,11 @@ const Footer = () => {
   return (
     <footer className="bg-bg-primary-clr-light dark:bg-bg-primary-clr-dark">
       <div className="px-4 py-3 text-center">
-        <p className="text-sm font-bold text-black dark:text-text-clr-primary">
+        <p className="text-sm text-black dark:text-text-clr-primary">
           Created with ❤️ by{" "}
-          <a href="https://github.com/iSubhamMani">Subham Mani</a>
+          <a className="font-bold" href="https://github.com/iSubhamMani">
+            Subham Mani
+          </a>
         </p>
       </div>
     </footer>
